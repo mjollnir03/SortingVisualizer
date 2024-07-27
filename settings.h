1adb6c21af1b5b67814606b563bedecfb9136471
Ellmaer Ranjber <ellmaer2003@gmail.com>
2024-07-27 11:02:30 -0700

    create settings file

---
+++ settings.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "raylib.h"
+
+
+const Color backgroundColor = GetColor(0xD6EFD8ff);
+const Color boldColor = GetColor(0x1A5319ff);
+
+
+const int screenWidth = 1600;
+const int screenHeight = 900;
+
