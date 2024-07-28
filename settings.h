a1bc63f1a2a337998cd554e31bc4193867726b79
Ellmaer <ellmaer2003@gmail.com>
2024-07-28 12:39:09 -0700

    update settings.h
    -added general font color for text

--- settings.h
+++ settings.h
@@ -4,6 +4,7 @@
 
 const Color backgroundColor = GetColor(0xD6EFD8ff);
 const Color boldColor = GetColor(0x1A5319ff);
+const Color fontColor = GetColor(0x508d4eff);
 
 
 const int screenWidth = 1600;
