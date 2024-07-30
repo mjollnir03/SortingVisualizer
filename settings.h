41ffee5a754fb8f2d2d308963a84c36c752e0ad8
Ellmaer <ellmaer2003@gmail.com>
2024-07-30 13:33:52 -0700

    Update settings.h

--- settings.h
+++ settings.h
@@ -1,12 +1,15 @@
 #pragma once
 #include "raylib.h"
+#include <string>
+#include <sstream>
 
+// Global Variables
 
+// Colors
 const Color backgroundColor = GetColor(0xD6EFD8ff);
 const Color boldColor = GetColor(0x1A5319ff);
 const Color fontColor = GetColor(0x508d4eff);
 
-
+// Screen sizes
 const int screenWidth = 1600;
 const int screenHeight = 900;
-
