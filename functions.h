9da7dd771973183caa9f50d120b3ad65ab080508
Ellmaer <ellmaer2003@gmail.com>
2024-07-30 13:21:04 -0700

    create functions.h
    -this header file will store the function definitions of said intermediate helper functions such as DisplayMousePosition()

---
+++ functions.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "raylib.h"
+
+// Function declarations
+// Function to help visualize spacing within application
+void DrawGridWithDots(int screenWidth, int screenHeight, int dotSpacing);
+// Function to help see current mouse position
+void DisplayMousePosition();
+void DrawCenteredTextWithButton(const char* text, int fontSize, int yPos, Color textColor, Color buttonColor, int buttonHeight, const char* buttonText);
