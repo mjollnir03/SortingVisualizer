df208d53efbdf33e6cd7b0db3c357c2b79788d3d
Ellmaer <ellmaer2003@gmail.com>
2024-08-01 14:10:29 -0700

    update titlescreen.h
    -need to move definitions to different files

--- TitleScreen.h
+++ TitleScreen.h
@@ -1,10 +1,54 @@
 #pragma once
 #include "settings.h"
-// Title Screen Class Definition
+// Title Screen Class Declarations
+// 
+// (Move definitions to "TitleScreen.cpp" file)
 
 class TitleScreen {
 private:
+	
+	struct TitleButton {
+	public:
+		int buttonHeight;
+		int buttonWidth;
+		const char* buttonText;
+		TitleButton(int height, int width, const char* text) {
+			this->buttonHeight = height;
+			this->buttonWidth = width;
+			this->buttonText = text;
+		}
+	};
+
+	struct TitleButton {
+	public:
+
+	};
+
+	struct TitleWaterMark {
+	public:
+
+	};
 
 public:
+	// Title Message variables
+	const char* titleText;
+	// Water mark 
+	const char* waterMark;
+	TitleButton* button;
+	
+	
+
+	TitleScreen() {
+		this->titleText = "Sorting Visualizer";
+		this->titleText = "- By Ellmaer Ranjber";
+		this->button = new TitleButton(10, 10, "Click to Start");
+	}
+
+	void DrawTitleScreen() {
+
+
+		// Draw Water Mark, bottom right of screen
+		DrawText("- By Ellmaer Ranjber", 1050, 840, 50, boldColor);
+	}
 
 };
