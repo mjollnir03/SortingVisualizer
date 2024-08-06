1fe4ad231b39c3794b5500bb32d40ed019de9303
Ellmaer <ellmaer2003@gmail.com>
2024-08-05 20:34:06 -0700

    code clean up

--- TitleScreen.h
+++ TitleScreen.h
@@ -1,70 +1,70 @@
 #pragma once
 #include "settings.h"
-// Title Screen Class Declarations
 
+// Title Screen Class Declarations
 class TitleScreen {
 private:
-	
-	class Button {
-	private:
-		// Button Rectangle 
-		int rectWidth;
-		int rectHeight;
-		int rectXPos;
-		int rectYPos;
-		Color rectangleColor;
 
-		//Button Text 
-		const char* buttonText;
-		int buttonTextWidth;
-		int buttonTextXPos;
-		int buttontextYPos;
-		int buttonFontSize;
-		Color buttonColor;
+    // Button Class Declaration
+    class Button {
+    private:
+        // Button Rectangle
+        int rectWidth;
+        int rectHeight;
+        int rectXPos;
+        int rectYPos;
+        Color rectangleColor;
 
-	public:
-		Button();
-		void DrawButton();
+        // Button Text
+        const char* buttonText;
+        int buttonTextWidth;
+        int buttonTextXPos;
+        int buttonTextYPos;
+        int buttonFontSize;
+        Color buttonColor;
 
-	};
+    public:
+        Button();           // Constructor
+        void DrawButton();  // Method to draw the button
+        bool buttonClicked(); // Method to see if title screen button is clicked
+    };
 
-	struct TitleText {
-	private:
-		const char* title;
-		int titleWidth;
-		int titleHeight;
-		int titleXPos;
-		int titleYPos;
-		int fontSize;
-		Color color;
-	public:
-		TitleText();
-		void DrawTitleText();
-		
-	};
+    // TitleText Struct Declaration
+    struct TitleText {
+    private:
+        const char* title;
+        int titleWidth;
+        int titleHeight;
+        int titleXPos;
+        int titleYPos;
+        int fontSize;
+        Color color;
 
-	struct TitleWaterMark {
-	private:
-		const char* text;
-		int XPos;
-		int YPos;
-		int fontSize;
-		Color color;
-	public:
-		TitleWaterMark();
-		void DrawTitleWaterMark();
+    public:
+        TitleText();             // Constructor
+        void DrawTitleText();    // Method to draw the title text
+    };
 
-	};
+    // TitleWaterMark Struct Declaration
+    struct TitleWaterMark {
+    private:
+        const char* text;
+        int XPos;
+        int YPos;
+        int fontSize;
+        Color color;
+
+    public:
+        TitleWaterMark();            // Constructor
+        void DrawTitleWaterMark();   // Method to draw the watermark
+    };
 
 public:
-	// Class variables here ...
-	Button* titleButton;
-	TitleScreen* titleText;
-	TitleWaterMark* titleWaterMark;
-	
-	
-	TitleScreen();
+    // Class variables
+    Button* titleButton;             // Pointer to Button
+    TitleText* titleText;            // Pointer to TitleText
+    TitleWaterMark* titleWaterMark;  // Pointer to TitleWaterMark
 
-	void DrawTitleScreen();
-
+    TitleScreen();              // Constructor
+    void DrawTitleScreen();     // Method to draw the entire title screen
 };
